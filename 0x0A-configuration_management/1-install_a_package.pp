181c3469a77bbffd0d64a6419ffc8153ab6d53aa
siphesihlemhlongo <sphesihlemhlongo07@gmail.com>
2023-11-25 00:33:27 +0000

    puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 #!/usr/bin/pup
 #install a specific version of flask(2.1.0)
-package {'flask':
+package{'flask':
 ensure => '2.1.0',
+provider => 'pip3',
+}
+
+package{'werkzeug':
+ensure => '2.1.1',
 provider => 'pip3',
 }
